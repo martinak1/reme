853a5602ac179666611b09f1fcece7ff0449504f
Alex Martin <martinak0@protonmail.com>
2019-05-10 16:32:35 -0400

    Pushing work on db.py and entry.py

--- reme/src/db.py
+++ reme/src/db.py
@@ -1,64 +1,90 @@
-from entry import Entry
-import logging
-import sqlite3
-import threading
-import time
+"""
+DB.py
+This handels the interaction between reme and the database. It can convert
+Entry objects to SQL statements and vice versa.
+"""
 
-"""
-TODO
-Add try blocks to all DB functions
-Add error recovery to all functions
-Add logging to all transactions
-"""
+import sqlite3
+import logging
+from datetime import datetime, timedelta
+from entry import Entry
+# import threading
+# import time
+
+###############################################################################
+# TODO
+# Add try blocks to all DB functions
+# Add error recovery to all functions
+# Add logging to all transactions
+##############################################################################
+
 
 class DB:
     """
-    Establishes a connection to the DB and sets a cursor
+    An object that handels interaction between reme and the DB
     """
+
     def __init__(self):
-        logging.info("db.py:__init__ - Attempting to establish a connection to the DB")
+        logging.info(
+            "db.py:__init__ - Attempting to establish a connection to the DB"
+        )
         self.connection = sqlite3.connect('reme.db')
         logging.info("db.py:__init__ - Connection established to DB")
-        logging.info("db.py:__init__ - Attempting to create a cursor in the DB")
-        self.cursor = self.connection.cursor()
-        logging.info("db.py:__init__ - Cursor created sucessfully")
-    # end __init__
 
+        self.make_table()
 
-    """
-    Creates the entry table if it does not already exist
-    """
-    def make_table(self):
+    def make_table(self) -> bool:
+        """
+j       Creates the entry table if it does not already exist
+        """
         try:
-            self.cursor.execute(
+            for row in self.connection.execute(
+                "select name from sqlite_master where type='table' \
+                and name not like 'sqlite_%';"):
+
+                if 'entries' in row:
+                    logging.info(
+                        "db.py:make_table - Entries table already exists"
+                    )
+                    return True
+
+            self.connection.execute(
                 """
                 CREATE TABLE entries (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    msg TEXT,
-                    users TEXT,
+                    uid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    msg TEXT NOT NULL,
+                    users TEXT NOT NULL,
                     attachments TEXT,
                     created DATETIME,
-                    executed DATETIME
+                    executed DATETIME NOT NULL
                 );
                 """
             )
-        except sqlite3.Warning:
-            logging.error("db.py:make_table - Failed to execute the `reme` table creation sql")
-            return False
 
-        logging.info("db.py:make_table - New `reme` table created")
+        except sqlite3.Warning:
+            logging.error(
+                "db.py:make_table [sqlite3.Warning] - Failed to execute the \
+                entries table creation sql"
+            )
+            return False
+        except sqlite3.OperationalError:
+            logging.error(
+                "db.py:make_table [sqlite3.OperationalError] - Failed to \
+                create the entries table. Table might already exist"
+            )
+
+        logging.info("db.py:make_table - New entries table created")
         return True
 
-
-    """
-    Insert an Entry object into the DB
-    """
-    def add_entry(self, entry):
+    def add_entry(self, entry: Entry) -> bool:
+        """
+        Insert an Entry object into the DB
+        """
         try:
-            self.cursor.execute(
+            self.connection.execute(
                 """
-                INSERT INTO entries( msg, users, attachments, created, executed )
-                VALUES( ?, ?, ?, ?, ? );
+                INSERT INTO entries(msg, users, attachments, created, executed)
+                VALUES(?, ?, ?, ?, ?);
                 """,
                 entry.msg(),
                 entry.flatten_users(),
@@ -73,31 +99,102 @@ class DB:
         logging.info("db.py:add_entry - Added an entry to the DB")
         return True
 
+    def close(self) -> bool:
+        """
+        Commits changes to the DB and closes the connection
+        TODO:
+        Add error handeling and error logging
+        """
+        logging.info(
+            "db.py:close - Starting to close the connection to the DB"
+        )
+        try:
+            self.connection.commit()
+            logging.info("db.py:close - Commited transactions to the DB")
+            self.connection.close()
+            logging.info("db.py:close - Connection to the DB has closed")
 
-    """
-    Commits changes to the DB and closes the connection
-    TODO:
-    Add error handeling and error logging
-    """
-    def close(self):
-        logging.info("db.py:close - Starting to close the connection to the DB")
-        self.connection.commit()
-        logging.info("db.py:close - Commited transactions to the DB")
-        self.connection.close()
-        logging.info("db.py:close - Connection to the DB has closed")
+        except sqlite3.Warning:
+            logging.info(
+                "db.py:close - Error occured while trying to close the \
+                connection to the DB"
+            )
+            return False
 
         return True
 
+    def collect(self, timestamp: datetime):
+        """
+        Collects the entries from the DB
+        :param timestamp datetime.datetime: A datetime to compare against the
+        executed column
+        :return collection list: The DB entries that match the given datetime
+        """
+        logging.info(
+            "db.py:collect - Collecting entries from the DB \
+            with timestamp {}".format(timestamp)
+        )
+        try:
+            return [row for row in self.connection.execute(
+                "SELECT * FROM entries WHERE executed=?", timestamp)]
+        except sqlite3.Warning:
+            logging.error(
+                "db.py:collect - Failed to retrieve entries from the DB"
+            )
+            return []
 
-    """
-    Collects the entries from the DB
-    """
-    def collect(self):
-        pass
+    def remove(self, uid: int) -> bool:
+        """
+        Remove entries from the DB
+        """
+        print("UID is {}".format(uid))
+        try:
+            logging.info(
+                "db.py:remove - Attempting to remove row with UID: {}".format(
+                    uid
+                )
+            )
+            self.connection.execute(
+                "delete from entries where uid=?;",
+                uid
+            )
 
+        except sqlite3.Warning:
+            logging.error(
+                "db.py:remove - Failed to remove row with UID: {}".format(
+                    uid
+                )
+            )
+            return False
 
-    """
-    Remove entries from the DB
-    """
-    def remove(self, id):
-        pass
+        return True
+
+    def gen_demo(self):
+        """
+        Creates dummy entries in the DB for testing purposes
+        """
+
+        created = datetime.now()
+
+        entries = [
+            ("Take pizza out of oven", "Jason", None, created,
+                created + timedelta(minutes=1)),
+            ("Go to the store for milk", "Patrick", None, created,
+                created + timedelta(minutes=2)),
+            ("Do laundry", "Alex", None, created,
+                created + timedelta(minutes=3)),
+            ("Workout!", "JB", None, created, created + timedelta(minutes=4)),
+            ("Walk around a little bit", "Alex", None, created,
+                created + timedelta(minutes=5)),
+            ("Smoke break!", "Alex", None, created,
+                created + timedelta(minutes=5))
+        ]
+
+        for row in entries:
+            self.connection.execute(
+                """
+                insert into entries(msg, users, attachments, created, executed)
+                values (?, ?, ?, ?, ?);
+                """,
+                row
+            )
--- reme/src/entry.py
+++ reme/src/entry.py
@@ -1,15 +1,144 @@
-#!/usr/bin/python3
+"""
+Entry - An intermediary object used to pass information between reme and the
+DB. Entry objects can be composed from either output from the DB or from a
+series of capture groups made when the msg_regex is used to parse a
+discord.Message.
+"""
 
 from datetime import datetime, timedelta
-from discord import Members, Message, User
+from discord import Member, Message  # , User
 import logging
 import re
 
-"""
-Takes the matches from msg_regex and creates a Datetime from
-the groups found
-"""
-def convert_date(matches):
+
+class Entry:
+    """
+    Entry - An object that is created by parsing messages reme recieves. Using
+    regular expressions, it extracts the message content, they date the
+    reminder should be sent, and the users who should receive the reminder.
+    """
+
+    msg_regex = re.compile(
+        r"!reme[ ]*(?P<message>.*)[ ]*((@[ ]*(?P<day>\d{1,2}[-\/]\d{1,2}){0,1}[ ]*(?P<time>\d{1,2}:\d{1,2}))|(?P<offset>\+[ ]*\d+[ ]*[DdHhMm]))"
+    )
+
+    def __init__(self, uid=None, msg="", users=None, attachments=None,
+                 created=None, executed=None):
+        self.uid = uid
+        self.msg = msg
+        self.users = users
+        self.attachments = attachments
+        self.created = created
+        self.executed = executed
+    # end __init__
+
+    def __str__(self):
+        return """
+        uid:            {}
+        msg:            {}
+        users:          {}
+        attachments:    {}
+        created:        {}
+        executed:       {}
+        """.format(self.uid, self.msg, self.users, self.attachments,
+                   self.created, self.executed)
+
+
+    def uid(self) -> int:
+        """
+        Return uid
+        """
+        return self.uid
+    # end uid
+
+    def msg(self):
+        """
+        Return msg
+        """
+        return self.msg
+    # end msg
+
+    def users(self) -> list:
+        """
+        Return users
+        :return list(discord.Users)
+        """
+        return self.users
+    # end users
+
+    def flatten_users(self) -> list:
+        """
+        Return users flattened
+        """
+        pass
+
+    def attachments(self) -> list:
+        """
+        Return attachments
+        """
+        return self.attachments
+    # end attachments
+
+    def flatten_attachments(self) -> list:
+        """
+        Return attachments flattened
+        """
+        pass
+
+    def created(self) -> datetime:
+        """
+        Return created
+        """
+        return self.created
+    # end created
+
+    def executed(self) -> datetime:
+        """
+        Return exectuted
+        """
+        return self.executed
+    # end executed
+
+
+def from_msg(message: Message) -> bool:
+    """
+    Sets the Entry attributes of the entry by collecting information
+    from a message
+    :param message discord.Message - A message object sent by discord
+    :return ent Entry or None
+    """
+
+    ent = Entry()
+    matches = Entry.msg_regex.match(message.content)
+
+    if matches:
+        ent.msg = matches.group('message')
+        ent.users = message.author + message.mentions
+        ent.attachments = message.attachments
+        ent.created = message.created_at
+        ent.executed = convert_date(matches)
+
+        logging.info(
+            "entry.py:from_msg - Entry created from message successfully"
+        )
+        return ent
+
+    logging.warning(
+        "entry.py:from_msg - Message does not match the required format"
+    )
+    return None
+    # end from_msg
+
+
+def convert_date(matches: re.Match) -> datetime:
+    """
+    Takes the matches from msg_regex and creates a Datetime from
+    the groups found
+    :param matches re.Matches: Capture groups returned by entry.msg_regex when
+    parsing a messgage
+    :return datetime.datetime: Datetime object composed with the catpure groups
+    present
+    """
     converted_date = datetime.toady()
     if matches.group('month'):
         converted_date.replace(month=matches.group('month'))
@@ -20,121 +149,32 @@ def convert_date(matches):
         converted_date.replace(minute=matches.group('min'))
 
     if matches.group('offset'):
-        if 'd' in matches.group('offset')[:1] or 'D' in matches.group('offset'):
+        if re.match(r'[Dd]{1}', matches.group('offset')[:1]):
             converted_date += timedelta(days=matches.group('offset')[:-1])
-        if 'h' in matches.group('offset')[:1] or 'H' in matches.group('offset'):
+        if re.match(r'[Hh]{1}', matches.group('offset')[:1]):
             converted_date += timedelta(hours=matches.group('offset')[:-1])
-        if 'm' in matches.group('offset')[:1] or 'M' in matches.group('offset'):
+        if re.match(r'[Mm]{1}', matches.group('offset')[:1]):
             converted_date += timedelta(minutes=matches.group('offset')[:-1])
 
     return converted_date
 
-# end convert_time
 
-class Entry:
-
-    msg_regex = re.compile(
-        r"!reme[ ]*(?P<message>.*)[ ]*((@[ ]*(?P<day>\d{1,2}[-\/]\d{1,2}){0,1}[ ]*(?P<time>\d{1,2}:\d{1,2}))|(?P<offset>\+[ ]*\d+[ ]*[DdHhMm]))"
-    )
-
-    def __init__(self):
-        id = None
-        msg = ""
-        users = ()
-        attachments = ()
-        created = None
-        executed = None
-    # end __init__
-
-
-    """
-    Return id
-    """
-    def id(self):
-        return self.id
-    # end id
-
-
-    """
-    Return msg
-    """
-    def msg(self):
-        return self.msg
-    # end msg
-
-
-    """
-    Return users
-    """
-    def users(self):
-        return self.users
-    # end users
-
-
-    """
-    Return users flattened
-    """
-    def flatten_users(self):
-        pass
-
-
-    """
-    Return attachments
-    """
-    def attachments(self):
-        return self.attachments
-    # end attachments
-
-
-    """
-    Return attachments flattened
-    """
-    def flatten_attachments(self):
-        pass
-
-
-    """
-    Return created
-    """
-    def created(self):
-        return self.created
-    # end created
-
-
-    """
-    Return exectuted
-    """
-    def executed(self):
-        return self.executed
-    # end executed
-
-
+def from_db(sql_output: tuple) -> Entry:
     """
     Create an Entry object from the results of a database query
+    :param sql_output tuple - A tuple representation of a row in the DB
+    :return Entry or None
     """
-    def from_db(self):
-        pass
-    # end from_db
+    logging.info(
+        "entry.py:from_db - Attempting to create an Entry object from DB \
+        output row={}".format(sql_output[0])
+    )
 
-
-    """
-    Sets the Entry attributes of the entry by collecting information
-    from a message
-    """
-    def from_msg(self, message):
-        matches = Entry.msg_regex.match(message.content)
-
-        if matches:
-            self.msg = matches.group('message')
-            self.users = message.author + message.mentions
-            self.attachments = message.attachments
-            self.created = message.created_at
-            self.executed = convert_date(matches)
-
-            logging.info("entry.py:from_msg - Entry created from message successfully")
-            return True
-
-        else:
-            logging.warning("entry.py:from_msg - Message does not match the required format")
-            return False
-    # end from_msg
+    return Entry(
+        uid=sql_output[0],
+        msg=sql_output[1],
+        users=sql_output[2],
+        attachments=sql_output[3],
+        created=sql_output[4],
+        executed=sql_output[5]
+    )
