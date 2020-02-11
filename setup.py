cdca0a14c15f1328cd0ccc320e27b262e4cb311d
Alexander Martin <martinak@SAF-DSA-IMAC.local>
2020-02-11 16:25:07 -0500

    Fixed issue where the README was not displaying on PyPi

--- setup.py
+++ setup.py
@@ -2,21 +2,25 @@ import pathlib
 from setuptools import setup
 
 # Get the dir housing the files
-cwd: str = str(pathlib.Path(__file__).parent)
-readme: str = f"{cwd}/README.md"
+cwd = pathlib.Path(__file__).parent
+readme = (cwd / "README.md").read_text()
 
 setup(
     name="reme",
-    version="1.0.0",
+    version="1.0.1",
     description="A Discord bot that reminds you of things",
     long_description=readme,
+    long_description_content_type="text/markdown",
     url="https://github.com/martinak1/reme",
     author="martinak1",
     license="BSD-3-Clause",
     classifiers=[
-        "License :: OSI Approved :: BSD 3 Clause License",
+        "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Communications :: Chat",
+        "Topic :: Games/Entertainment"
     ],
     packages=["reme"],
     include_package_data=True,
